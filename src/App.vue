ae1b0cab728df321ba236c8c4bae548657f75318
kevbar98 <kevbar_98@hotmail.com>
2024-03-29 20:34:59 -0500

    Login completo

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+/* Importing fonts from Google */
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap');
+@import url('https://use.fontawesome.com/releases/v5.7.2/css/all.css');
+/* Reseting */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif;
+}
+
+body {
+    background: #ecf0f3;
+}
+</style>
